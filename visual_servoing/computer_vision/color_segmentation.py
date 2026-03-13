113d52a2e755dca616e81fe6559d6c455466c047
Weiming Zhou <weimingchess108@gmail.com>
2026-03-13 08:37:41 +0000

    Add first implementation of color segmentation. Lowest score on a test case is ~.6

--- visual_servoing/computer_vision/color_segmentation.py
+++ visual_servoing/computer_vision/color_segmentation.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
 #################### X-Y CONVENTIONS #########################
@@ -14,14 +14,58 @@ import numpy as np
 ###############################################################
 
 
+
 def image_print(img):
     """
     Helper function to print out images, for debugging. Pass them in as a list.
     Press any key to continue.
     """
-    cv2.imshow("image", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    cv.imshow("image", img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+def preprocess(img, template):
+    """
+    Preprocess image to eliminate outliers and smooth corners
+    """
+    return img
+
+
+def get_mask(img):
+    """
+    Determine a mask of points with color with sufficiently close hue to hue
+    """
+
+    hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+
+    low = np.array([0, 180, 180])
+    high = np.array([30, 255, 255])
+
+    mask = cv.inRange(hsv, low, high)
+
+    return mask
+
+
+def get_bounding_box(mask):
+    """
+    Find the bounding box of a mask
+    Returns ((top left), (bottom right)) in the form x1,y1,x2,y2
+    """
+    
+    
+    contours, hiearchy = cv.findContours(mask, 1, 2)
+
+    cnt = max(contours, key=cv.contourArea)
+    # find the largest contour
+
+    x,y,w,h = cv.boundingRect(cnt) 
+
+    # cv.rectangle(mask,(x,y),(x+w,y+h),(0,255,0),2)
+    
+    # image_print(mask)
+
+    return (x, y, x+w, y+h)
+
 
 
 def cd_color_segmentation(img, template):
@@ -36,7 +80,19 @@ def cd_color_segmentation(img, template):
     """
     ########## YOUR CODE STARTS HERE ##########
 
-    bounding_box = ((0, 0), (0, 0))
+
+
+    mask = get_mask(img)
+
+    # masked_img = cv.bitwise_and(img, img, mask=mask)
+
+    # image_print(mask)
+    # image_print(masked_img)
+
+    x1,y1,x2,y2 = get_bounding_box(mask)
+
+    bounding_box = ((x1, y1), (x2, y2))
+
 
     ########### YOUR CODE ENDS HERE ###########
 
